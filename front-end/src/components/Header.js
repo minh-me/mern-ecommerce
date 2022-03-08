1e3e8c139ae30c6bb65fac032d1a2ed3c691859d
minh chiu <minh.mchiu@gmail.com>
2022-03-08 20:57:48 +0700

    Update logo

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -32,7 +32,7 @@ export const Header = () => {
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
           <Container>
             <LinkContainer to='/'>
-              <Navbar.Brand>ProShop</Navbar.Brand>
+              <Navbar.Brand>Online Shop</Navbar.Brand>
             </LinkContainer>
             <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
             <Navbar.Collapse id='basic-navbar-nav'>
